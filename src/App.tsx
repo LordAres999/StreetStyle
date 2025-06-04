d1aa86e3984ab47c90f0c1ff5b992b9066aca45e
LordAres999 <ezem77743@gmail.com>
2025-06-04 11:50:19 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,14 @@ import WishlistPage from "./pages/WishlistPage";
 import SearchPage from "./pages/SearchPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminProductEdit from "./pages/admin/AdminProductEdit";
-import AdminProductNew from "./pages/admin/AdminProductNew";
-import AdminProductList from "./pages/admin/AdminProductList";
 import AccountPage from "./pages/AccountPage";
 import FAQPage from "./pages/FAQPage";
 import ReturnsPage from "./pages/ReturnsPage";
 import ContactPage from "./pages/ContactPage";
 import SizeGuidePage from "./pages/SizeGuidePage";
-import AdminOrderList from "./pages/admin/AdminOrderList";
-import AdminOrderDetail from "./pages/admin/AdminOrderDetail";
-import UserOrdersPage from "./pages/orders"; // Corrected import path
-import UserOrderDetailPage from "./pages/OrderDetailPage"; // Corrected import path
+import UserOrdersPage from "./pages/orders";
+import UserOrderDetailPage from "./pages/OrderDetailPage";
 import TrackOrderPage from "./pages/TrackOrderPage";
-import AdminUserList from "./pages/admin/AdminUsersList"; 
 
 const App = () => {
   return (
@@ -57,7 +50,6 @@ const App = () => {
           <AccountPage />
         </ProtectedRoute>
       } />
-      {/* User Orders Routes */}
       <Route path="/dashboard/orders" element={
         <ProtectedRoute>
           <UserOrdersPage />
@@ -68,7 +60,6 @@ const App = () => {
           <UserOrderDetailPage />
         </ProtectedRoute>
       } />
-      {/* Public Track Order Route */}
       <Route path="/track-order" element={<TrackOrderPage />} />
       <Route path="/profile" element={
         <ProtectedRoute>
@@ -82,38 +73,6 @@ const App = () => {
       <Route path="/returns" element={<ReturnsPage />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path="/size-guide" element={<SizeGuidePage />} />
-      {/* Admin Routes */}
-      <Route path="/admin" element={
-        <ProtectedRoute requireAdmin={true}>
-          <AdminDashboard />
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/products" element={
-        <ProtectedRoute requireAdmin={true}>
-          <AdminProductList />
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/products/:productId" element={
-        <ProtectedRoute requireAdmin={true}>
-          <AdminProductEdit />
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/products/new" element={
-        <ProtectedRoute requireAdmin={true}>
-          <AdminProductNew />
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/orders" element={
-        <ProtectedRoute requireAdmin={true}>
-          <AdminOrderList />
-        </ProtectedRoute>
-      } />
-      <Route path="/admin/orders/:orderId" element={
-        <ProtectedRoute requireAdmin={true}>
-          <AdminOrderDetail />
-        </ProtectedRoute>
-      } />
-      <Route path="users" element={<AdminUserList />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
